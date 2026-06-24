20a4e6c59a42155ea49575c3572093ce7c1a2052
Fuzialshaikh <fuzelytstatus@gmail.com>
2026-06-25 04:31:08 +0530

    Update config_template.py

--- config_template.py
+++ config_template.py
@@ -1,8 +1,8 @@
-# config.py
+config_template.py
 
 OVERSEERR_API_URL = 'http://YOUR_IP_ADDRESS:5055/api/v1'
 OVERSEERR_API_KEY = 'YOUR_OVERSEERR_API_KEY'
-TELEGRAM_TOKEN = 'YOUR_TELEGRAM_TOKEN'
+TELEGRAM_TOKEN = 8931105254:AAG1qCOBv-n59PahcWWyL-6EtdO4F0mifUA
 
 # Access Control Configuration
 # Set a password to protect access. If empty, no access control is applied.
